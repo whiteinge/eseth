23e1e4b051ffd6418f143d4a7b8b6392f55048d6
Seth House <seth@eseth.com>
2009-11-21 21:40:37 -0700

    Initial stab at using Sphinx to generate the blog.

--- uimodules.py
+++
@@ -1,7 +0,0 @@
-import tornado.web
-
-class Entry(tornado.web.UIModule):
-    def render(self, entry, show_comments=False):
-        return self.render_string(
-            'module-entry.html', show_comments=show_comments)
-

9fb30074e56c8d89d3cdf53f5a9fd4fa275c2728
Seth House <seth@eseth.com>
2010-01-05 11:51:15 -0700

    No reason to split a small index among many pages.
    --HG--
    extra : rebase_source : 75566df30a7db5e7c07709d1a9f5cfac6f0599c0

--- conf.py
+++ conf.py
@@ -171,7 +171,7 @@ html_use_smartypants = True
 #html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-html_split_index = True
+# html_split_index = True
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = True

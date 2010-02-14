b3f6b2e192b06bc71cbf60891b21f2e0962972b5
Seth House <seth@eseth.com>
2010-02-14 12:21:34 -0700

    Added link to Google Profile and changed Copyright name.

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Esoteric Rubbish'
-copyright = u'2009, Seth House'
+copyright = u'2009, Nathaniel Whiteinge'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

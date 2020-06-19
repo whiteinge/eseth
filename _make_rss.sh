ac252e8585f3524d45304c13253de7244bab86e2
Seth House <seth@eseth.com>
2020-06-18 22:21:24 -0600

    Update RSS generation to use m4

--- _make_rss.sh
+++ _make_rss.sh
@@ -15,20 +15,16 @@ printf '<?xml version="1.0" encoding="UTF-8"?>
     <ttl>40</ttl>
 ' "$(date -R)"
 
-< _metadata_cache sort -r -k2 | head -10 \
+< _metadata_cache sort -r -k2 | grep -v xDATE | head -10 \
 | while IFS="$tab" read -r pfile pdate ptitle pcategories; do
+    fname=$(printf "%s\n" "$pfile" | sed -e 's/\.md$/.mdhtml/')
+    fpath=$(dirname "$pfile")
     printf '<item>\n'
         printf '<title>%s</title>\n' "$ptitle"
         printf '<link>xROOT/%s</link>\n' "$pfile"
         printf '<description><![CDATA[\n'
-	PANDOC_PREFIX="xROOT/${pfile%/*}" pandoc \
-            --from=markdown \
-            --to html \
-            --lua-filter ./_absolute_links.lua \
-            --lua-filter ./_run.lua \
-            --lua-filter ./_include.lua \
-            --lua-filter ./_title-to-meta-title.lua \
-            $(printf "$pfile" | sed -e 's/\.html$/.md/')
+        cat "$fname" \
+            | sed -e 's/src="\.\//src="xROOT\/'"$fpath"'\//g'
         printf ']]></description>\n'
         printf '<pubDate>%s</pubDate>\n' "$(date -d"$pdate" -R)"
         printf '<guid>xROOT/%s</guid>\n' "$pfile"
